47b98eaf87df0f982f28d80f3c63f83727774f48
Scott <scott.tolksdorf@gmail.com>
2013-10-11 03:01:29 -0400

    Moved around prototypes and cleaned up the html a bit

--- public/js/engine/CalculatorBlueprintModel.js
+++ public/js/engine/CalculatorBlueprintModel.js
@@ -2,19 +2,9 @@
 /**
  * This is what get fecthed and uploaded to the server
  * contains the blueprint for generating the actual Calculator Model
- */
+
 Presto_Model_CalculatorBlueprint = XO.Model.extend({
 	urlRoot : '/api/calculators',
-
-	/*
-	defaults : {
-		title       : 'New Calculator',
-		description : 'Click here to edit',
-		icon        : 'icon-file-alt',
-		color       : 'yellow',
-		script      : "{\n	title       : 'New Calculator',\n	description : 'Click to edit',\n	icon        : 'icon-file-alt',\n	color       : 'yellow',\n\n	inputs : {\n		capital : {\n			title : 'Capital',\n			description : 'How much money ya got',\n			type  : Type.Money,\n			initialValue : 500\n		},\n		age : {\n			title : 'Age',\n			type  : Type.Number,\n			initialValue : 25\n		}\n	},\n\n	tables : {\n		sample : {\n			title : 'Sample Table',\n			columns : {\n				age : {\n					title : 'Age',\n					type : Type.Number,\n					firstCell : function(){\n						return Inputs.age;\n					},\n					generator : function(previousCellValue, index){\n						return previousCellValue + 1;\n					}\n				},\n				capitalDelta : {\n					title : 'Capital Change',\n					type  : Type.Money,\n					firstCell : function(){\n						return Inputs.capital;\n					},\n					generator : function(previousCellValue, index){\n						return previousCellValue + Math.random()*100;\n					}\n				},\n				random : {\n					title : 'Random',\n					type  : Type.Money,\n					firstCell : function(){\n						return 0;\n					},\n					generator : function(previousCellValue, index){\n						return previousCellValue + Math.random()*100;\n					}\n				},\n			}\n		}\n	},\n\n	charts : {\n		basic : {\n			title : function(){\n				return 'Sample Chart' + Outputs.test;\n			},\n			hover : function(x,y,label){\n				return '<b>$' + y + '</b> at month ' + x + '</br>' + label;\n			},\n			breakeven : [ ['capitalDelta', 'random'] ],\n			table : 'sample',\n		}\n	},\n\n	outputs : {\n		breakeven : {\n			title : 'Break-even Age',\n			description : 'Age at which you made 10% of your initial capital',\n			type : Type.Number,\n			value : function(){\n				var breakEvenAge = Tables.sample.age.find(function(age, index){\n					if(Tables.sample.capitalDelta[index] > Inputs.capital * 1.10){\n						return true;\n					}\n					return false;\n				});\n				return breakEvenAge;\n			}\n		}\n	}\n}\n"
-	},
-	*/
 
 	upload : function(callback)
 	{
@@ -47,10 +37,6 @@ Presto_Model_CalculatorBlueprint = XO.Model.extend({
 	},
 
 
-	/**
-	 * Executes the current script and triggers out the resultant object
-	 * @return {[type]} [description]
-	 */
 	execute : function(callback)
 	{
 		var self = this;
@@ -73,7 +59,7 @@ Presto_Model_CalculatorBlueprint = XO.Model.extend({
 	},
 
 });
-
+*/
 
 
 
