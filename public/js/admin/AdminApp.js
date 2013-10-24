25e710ae735b1c73f73750cb0784ebba399f644a
Scott <scott.tolksdorf@gmail.com>
2013-10-24 14:59:47 -0400

    Added a backup and restore button to the admin page for mass modfying calc scripts

--- public/js/admin/AdminApp.js
+++ public/js/admin/AdminApp.js
@@ -5,38 +5,78 @@ PrestoAdmin = xo.view.extend({
 		var self = this;
 
 		_.each(endpoints, function(ep){
-			var model;
+			var collection;
 			if(ep === '/api/calculators'){
-				model = Presto_Model_Calculator;
+				collection = xo.collection.extend({
+					model : Presto_Model_Calculator
+				});
+				self.calculators = collection
 			}else {
-				model = xo.model.extend({
-					urlRoot : ep
+				collection = xo.collection.extend({
+					model : xo.model.extend({
+						urlRoot : ep
+					})
 				});
 			}
 
-			var col = xo.collection.extend({
-				model : model
-			});
-
-			var group = Presto_View_EndpointGroup.create(col);
-			group.injectInto($('.routes'));
-			col.fetch();
+			var group = Presto_View_EndpointGroup.create(collection);
+			group.injectInto($('.routes__container'));
+			collection.fetch();
 		});
 
 		this.content = Presto_View_Content.create();
 
+
+
+		//Loads All the calculator scripts to text area
+		$('.BACKUP').click(function(){
+
+			self.content.dom.buttons.hide()
+			self.content.dom.data.val(
+				"[" + _.map(self.calculators, function(calc){
+					return calc.script;
+				}) + "]"
+				);
+
+			self.content.resize();
+		});
+
+		$('.RESTORE').click(function(){
+
+			if(!confirm("Delete all current calcaultors and restore with code in box?")){
+				return;
+			}
+			var calcData = parseText(self.content.dom.data.val());
+
+			if(calcData.length === 0){
+				alert('No restore calculator data found');
+				return;
+			}
+
+			console.log(calcData);
+
+		});
+
+
 		return this;
 	},
 
+
+
+
+
 	loadContent : function(newContent){
-
 		this.content.load(newContent);
-
 		return this;
 	},
 
 
 });
+
+
+
+
+
 
 parseText = function(text){
 	var result = [];
